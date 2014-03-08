d913b5cb3b25f8333076becb5de93f483f5f3c9a
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-08 14:40:02 -0700

    02-a: Add file chooser to root page.
    * In spec file: added ":type => :feature do" to allow capybara methods in rspec tests.

--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
-describe "Root path" do
+describe "Root path", :type => :feature do
   it "should return a valid response when visiting the root path" do
     get '/'
     last_response.should be_successful
   end
+
+  it "should have an upload button" do
+    visit '/'
+    expect(page).to have_field('video', :type => 'file')
+  end
+
 end
