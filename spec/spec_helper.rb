ddedb9c5f3bd9ff7148a4259ff3b959ae03c87da
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-07 17:52:45 -0700

    01-c: Add Rspec and Capybara for testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $LOAD_PATH.unshift(File.expand_path('.'))
-require "rspec"
+require 'groundhog_app'
+require 'rspec'
+require 'capybara/rspec'
+
+Capybara.app = Sinatra::Application
+
+def app
+  Sinatra::Application
+end
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
