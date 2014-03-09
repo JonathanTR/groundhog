efd1edd5e9c9a33fc0ca56aeb2ce6d9e0faec514
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-09 15:53:47 -0600

    02-e: Set up path names for test files and clear out test outputs after every test.

--- spec/models/video_converter_spec.rb
+++ spec/models/video_converter_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe "VideoConverter" do
   let(:video_path){'spec/resources/grasshopper_test.mp4'}
-  let(:video_name){'grasshopper_test.mp4'}
-  let(:temp_input_path){"public/temp_input/#{video_name}"}
+  let(:video_name){'grasshopper_test'}
+  let(:temp_input_path){"public/temp_input/#{video_name}.mp4"}
+  let(:temp_output_path){"public/temp_output/#{video_name}.gif"}
+
+  after :each do
+    File.delete(temp_input_path) if File.exist?(temp_input_path)
+    File.delete(temp_output_path) if File.exist?(temp_output_path)
+  end
 
   it "should accept a video path" do
     expect(VideoConverter.convert_to_gif(video_path)).not_to raise_error
