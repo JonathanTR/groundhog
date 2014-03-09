9a71a2f06ca8cd409e1b89491f79b1f52e32f48e
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-09 14:25:14 -0600

    02-d: Extract visit '/' to before :each in user_uploads_a_file.
    * Just a little refactoring.

--- spec/features/user_uploads_a_file_spec.rb
+++ spec/features/user_uploads_a_file_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
 
 feature "User clicks upload" do
-  
-  scenario "and attached a valid file" do
+  let(:video_path){'spec/resources/grasshopper_test.mp4'}
+  let(:video_name){'grasshopper_test.mp4'}
+  let(:temp_input_path){"public/temp_input/#{video_name}"}
+
+  before :each do
     visit '/'
+  end
+
+  scenario "and attached a valid file" do
     page.attach_file('video', 'spec/resources/grasshopper_test.mp4')
     click_button("Upload!")
     expect(page.status_code).to be(200)
   end
 
   scenario "but did not attach a file", js: true do
-    visit '/'
     click_button("Upload!")
     expect(page).to have_content('Please choose a file to upload.')
   end
 
   scenario "but attached an invalid file type" do
-    visit '/'
     page.attach_file('video', 'spec/resources/invalid.txt')
     click_button("Upload!")
     expect(page.status_code).to be(406)
