ef9d6c3e144f79f06fad4e9874d18acb8bb0ce60
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-09 13:52:25 -0600

    02-d: Add copy_to_temp_input to VideoConverter class
    * I explored the idea of just using the tempfile created on upload to perform the transformations, but settled on the idea of copying it to a temp directory for a couple of reasons, mainly that it will be easier to grab the file and display it for a user if I move this to a two step process in the future. Second, because it is not totally clear where the tempfile created on upload ends up when the code is pushed to the server.

--- models/video_converter.rb
+++ models/video_converter.rb
@@ -1,5 +1,9 @@
 class VideoConverter
 
+  def self.copy_to_temp_input(target_path, file_path)
+    File.open(target_path, 'w'){|f| f.write File.read(file_path)}
+  end
+
   def self.makeGif(video_path)
   end
   
--- spec/models/video_converter_spec.rb
+++ spec/models/video_converter_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 describe "VideoConverter" do
   let(:video_path){'spec/resources/grasshopper_test.mp4'}
+  let(:video_name){'grasshopper_test.mp4'}
+  let(:temp_input_path){"public/temp_input/#{video_name}"}
 
   it "should accept a video path" do
     expect(VideoConverter.makeGif(video_path)).not_to raise_error
+  end
+
+  it "should copy the video file to the temp_input folder" do
+    VideoConverter.copy_to_temp_input(temp_input_path, video_path)
+    original = File.read(video_path)
+    result = File.read(temp_input_path)
+    expect(original).to eq(result)
+    File.delete(temp_input_path)
   end
 end
