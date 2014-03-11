4f41f8ac44a262762b3cd54b86c58fa890f85acf
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-10 18:00:17 -0600

    Change target_path to video_storage_path and gif_path to gif_storage_path for readability.

--- groundhog_app.rb
+++ groundhog_app.rb
@@ -16,11 +16,11 @@ post '/upload' do
     gif_start_point = params["start-time"].to_i
     gif_duration = params["end-time"].to_i - gif_start_point
     video_title = strip_filetype(filename)
-    target_path = "public/temp_video/#{filename}"
-    gif_path = "public/temp_gif/#{video_title}.gif"
-    VideoConverter.copy_to_temp_video(target_path, file_source.path)
-    VideoConverter.convert_to_gif(gif_path, target_path, gif_start_point, gif_duration)
-    @gif_path = gif_path.gsub!("public/", "")
+    video_storage_path = "public/temp_video/#{filename}"
+    gif_storage_path = "public/temp_gif/#{video_title}.gif"
+    VideoConverter.copy_to_temp_video(video_storage_path, file_source.path)
+    VideoConverter.convert_to_gif(gif_storage_path, video_storage_path, gif_start_point, gif_duration)
+    @gif_storage_path = gif_storage_path.gsub!("public/", "")
     @gif_title = "#{video_title}.gif"
     erb :download
   end
