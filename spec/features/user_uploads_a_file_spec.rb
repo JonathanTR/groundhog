26aaf3faeabe8f25a7a8657fa11e61877917c24f
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-11 12:48:11 -0600

    Remove unnecessary puts statement.

--- spec/features/user_uploads_a_file_spec.rb
+++ spec/features/user_uploads_a_file_spec.rb
@@ -31,7 +31,6 @@ feature "User clicks upload" do
   scenario "the file should be saved to a temp_video folder" do
     page.attach_file('video', 'spec/resources/grasshopper_test.mp4')
     click_button("Make a gif!")
-    puts temp_video_path
     expect(File.exist?(temp_video_path)).to be_true
     File.delete(temp_video_path)
   end
