153486cb36e472e79b02f7130283a53fd563a57f
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-11 14:37:21 -0600

    08-i: The name of the file should be passed through the form on the preview page.
    * This is sort of a workaround for maintaining state in this app. It may be replaced at a later point, but for right now, it makes sense because it is fairly simple.

--- groundhog_app.rb
+++ groundhog_app.rb
@@ -12,11 +12,13 @@ post '/upload' do
     erb :error_406
   else
     file_source = params["video"][:tempfile]
-    filename = params["video"][:filename]
-    video_storage_path = "public/temp_video/#{filename}"
+    @filename = params["video"][:filename]
+    video_storage_path = "public/temp_video/#{@filename}"
     VideoConverter.copy_to_temp_video(video_storage_path, file_source.path)
     @video_storage_link = strip_public_folder(video_storage_path)
     erb :preview
+  end
+end
 
     gif_storage_path = "public/temp_gif/#{strip_filetype(filename)}.gif"
     gif_start_point = params["start-time"].to_i
